f9be9b3df5364e0f53d22b74f335c016ddb5a334
Jay Newstrom <jaynewstrom@gmail.com>
2019-03-31 13:24:15 -0500

    Convert to AndroidX
    Closes #98

--- screen-switcher-sample/src/androidTest/java/com/jaynewstrom/screenswitchersample/activity/MainActivityTest.kt
+++ screen-switcher-sample/src/androidTest/java/com/jaynewstrom/screenswitchersample/activity/MainActivityTest.kt
@@ -1,14 +1,14 @@
 package com.jaynewstrom.screenswitchersample.activity
 
 import android.animation.ValueAnimator
-import android.support.test.espresso.Espresso.onView
-import android.support.test.espresso.Espresso.pressBack
-import android.support.test.espresso.action.ViewActions.click
-import android.support.test.espresso.assertion.ViewAssertions.matches
-import android.support.test.espresso.matcher.ViewMatchers.isDisplayed
-import android.support.test.espresso.matcher.ViewMatchers.withId
-import android.support.test.rule.ActivityTestRule
-import android.support.test.runner.AndroidJUnit4
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import com.jaynewstrom.screenswitchersample.R
 import org.junit.Before
 import org.junit.Rule
