f1b6f5f884e2fe17b0a39602cc99a03923d110fb
Jay Newstrom <jaynewstrom@gmail.com>
2020-07-01 11:41:14 -0500

    Try catch isn't needed. (#139)

--- espresso-testing-bootstrap/src/main/java/screenswitchersample/espressotestingbootstrap/activity/TestInitialScreenFactory.kt
+++ espresso-testing-bootstrap/src/main/java/screenswitchersample/espressotestingbootstrap/activity/TestInitialScreenFactory.kt
@@ -8,12 +8,8 @@ import javax.inject.Inject
 
 internal class TestInitialScreenFactory @Inject constructor() : InitialScreenFactory {
     override fun create(intent: Intent?): List<Screen> {
-        try {
-            val screenClassName = intent!!.getStringExtra(TEST_SCREEN_FACTORY)!!
-            val screenFactory = Class.forName(screenClassName).newInstance() as TestScreenFactory
-            return listOf(screenFactory.createScreen())
-        } catch (e: Exception) {
-            throw RuntimeException(e)
-        }
+        val screenClassName = intent!!.getStringExtra(TEST_SCREEN_FACTORY)!!
+        val screenFactory = Class.forName(screenClassName).newInstance() as TestScreenFactory
+        return listOf(screenFactory.createScreen())
     }
 }
