b1ad35ce11e8257519426b56e7003d232b379ef5
Jay Newstrom <jaynewstrom@gmail.com>
2020-07-09 14:06:40 -0500

    Add the ability to prevent a screen switcher from finishing. (#142)

--- tab-bar/src/main/java/com/jaynewstrom/screenswitcher/tabbar/TabBarItem.kt
+++ tab-bar/src/main/java/com/jaynewstrom/screenswitcher/tabbar/TabBarItem.kt
@@ -17,30 +17,27 @@ import io.reactivex.Observable
 import screenswitchersample.core.view.inflate
 
 class TabBarItem(
-    screen: Screen,
+    private val initialScreenFactory: () -> Screen,
     private val title: String,
     @DrawableRes private val icon: Int,
     internal val badgeCountObservable: Observable<Int>? = null
 ) {
-    private var screen: Screen? = screen
     private var screenSwitcherState: ScreenSwitcherState? = null
 
     internal fun createIndicatorView(hostView: ViewGroup): BottomNavItemLayout {
         return BottomNavItemLayout(hostView.context, icon, title)
     }
 
-    internal fun createContentView(hostView: ViewGroup): View {
+    internal fun createContentView(hostView: ViewGroup, presenter: TabBarPresenter): View {
         val state: ScreenSwitcherState = screenSwitcherState
-            ?: hostView.screenSwitcherState().createNestedState(listOf(screen!!)).also {
+            ?: hostView.screenSwitcherState().createNestedState(listOf(initialScreenFactory())).also {
                 screenSwitcherState = it // Assign the state to the field so we don't have to create it.
-                screen = null // Clear the screen so it doesn't leak.
             }
 
         val screenHost = hostView.inflate(R.layout.tab_root) as StateAwareFrameLayout
         screenHost.isSaveFromParentEnabled = false
 
-        val message = "Nested Screen Switchers cannot have zero screens. Handle this in the parent screen switcher."
-        val finishHandler = IllegalStateExceptionScreenSwitcherFinishHandler(message)
+        val finishHandler = TabScreenSwitcherFinishHandler(presenter)
         val screenSwitcher = ScreenSwitcherFactory.viewScreenSwitcher(screenHost, state, finishHandler)
 
         screenHost.saveStateDelegate = object : StateAwareFrameLayout.SaveStateDelegate {
@@ -52,14 +49,12 @@ class TabBarItem(
         return screenHost
     }
 
-    internal fun contentViewHandlesPop(contentView: View): Boolean {
-        val screenSwitcherState = contentView.getTag(R.id.screen_switcher_state) as ScreenSwitcherState
-        if (screenSwitcherState.screenCount() <= 1) {
-            return false
-        }
+    internal fun handleScreenPop(contentView: View) {
         val screenSwitcher = contentView.getTag(R.id.screen_switcher) as ScreenSwitcher
+        if (screenSwitcher.isTransitioning) {
+            return
+        }
         screenSwitcher.pop(1)
-        return true
     }
 
     internal fun popContentToRoot(contentView: View) {
@@ -71,13 +66,30 @@ class TabBarItem(
         val screenSwitcher = contentView.getTag(R.id.screen_switcher) as ScreenSwitcher
         screenSwitcher.pop(screenSwitcherState.screenCount() - 1)
     }
-}
 
-private class IllegalStateExceptionScreenSwitcherFinishHandler(
-    private val message: String
-) : ScreenSwitcherFinishHandler {
-    override fun onScreenSwitcherFinished(finishCompleteHandler: ScreenSwitcherFinishHandler.FinishCompleteHandler) {
-        throw IllegalStateException(message)
+    private inner class TabScreenSwitcherFinishHandler(
+        private val presenter: TabBarPresenter
+    ) : ScreenSwitcherFinishHandler {
+        override val screenSwitcherShouldFinish: Boolean
+            get() {
+                val currentTabBarItem = presenter.currentTabBarItemStateHolder.state
+                val tabBarItems = presenter.tabBarItems
+                // Never remove the zero indexed tab completely.
+                // It should remain while the exit confirmation is displayed, or if the exit confirmation is cancelled.
+                val isFirstTab = tabBarItems.indexOf(currentTabBarItem) == 0
+                if (isFirstTab) {
+                    presenter.finishFirstTabCalled()
+                }
+                return !isFirstTab
+            }
+
+        override fun onScreenSwitcherFinished(
+            finishCompleteHandler: ScreenSwitcherFinishHandler.FinishCompleteHandler
+        ) {
+            screenSwitcherState = null
+            presenter.removeCurrentContentView()
+            finishCompleteHandler.finishComplete()
+        }
     }
 }
 
